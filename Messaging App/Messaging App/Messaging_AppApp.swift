863bce1c6238aebaef992dba419ef7b4b1ba19e8
nolasco-ever <74639646+nolasco-ever@users.noreply.github.com>
2021-12-20 20:15:01 -0500

    Create iOS app

---
+++ Messaging App/Messaging App/Messaging_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Messaging_AppApp.swift
+//  Messaging App
+//
+//  Created by Ever Nolasco on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Messaging_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
