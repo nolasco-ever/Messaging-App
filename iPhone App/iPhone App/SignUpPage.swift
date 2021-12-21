2b41ea8dd52997a963c1ec42fac639f9215386cb
nolasco-ever <74639646+nolasco-ever@users.noreply.github.com>
2021-12-21 02:30:59 -0500

    Successful sign up
    Firebase implemented
    Sign up page functional

--- iPhone App/iPhone App/SignUpPage.swift
+++ iPhone App/iPhone App/SignUpPage.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignUpPage: UIViewController {
+    let auth = Auth.auth()
     
     let radius = 10
     
@@ -25,12 +27,32 @@ class SignUpPage: UIViewController {
         passwordTextField.layer.cornerRadius = CGFloat(radius)
         signUpButton.layer.cornerRadius = CGFloat(radius)
     }
-
+    
+    @IBAction func createUser(_ sender: Any) {
+        let name: String = fullNameTextField.text!
+        let email: String = usernameTextField.text!
+        let password: String = passwordTextField.text!
+        
+        signUp(name: name, email: email, password: password)
+        
+        
+    }
+    
     @IBAction func goBack(_ sender: Any) {
         guard let loginVC = storyboard?.instantiateViewController(withIdentifier: "login_vc") as? LoginPage else { return }
         
         loginVC.modalPresentationStyle = .fullScreen
         
         present(loginVC, animated: true)
+    }
+    
+    func signUp(name: String, email: String, password: String){
+        auth.createUser(withEmail: email, password: password){ result, error in
+            guard result != nil, error == nil else{
+                return
+            }
+            
+//            Success
+        }
     }
 }
